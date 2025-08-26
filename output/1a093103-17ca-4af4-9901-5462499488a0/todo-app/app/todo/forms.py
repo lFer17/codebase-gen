4ff60191ea58751fda3547ce00176c20b8241d14
lfer17 <lxavez20@gmail.com>
2025-08-25 21:22:32 -0400

    Remove output folder from repo tracking

--- output/1a093103-17ca-4af4-9901-5462499488a0/todo-app/app/todo/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length
-
-class TodoForm(FlaskForm):
-    title = StringField("Title", validators=[DataRequired(), Length(min=1, max=140)])
-    description = TextAreaField("Description")
-    completed = BooleanField("Completed")
-    submit = SubmitField("Submit")
--- output/1a093103-17ca-4af4-9901-5462499488a0/todo-app/run.py
+++
@@ -1,8 +0,0 @@
-import os
-from app import create_app
-
-config_name = os.environ.get("FLASK_ENV") or "development"
-app = create_app(config_name)
-
-if __name__ == "__main__":
-    app.run()
